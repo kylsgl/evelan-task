2fc3f589e19eafa82c624669e637b018531dc060
kylsgl <kylsgl4@Gmail.com>
2023-04-24 14:26:25 +0800

    refactor(getUser): Add json validation

--- src/services/api/getUsers.ts
+++ src/services/api/getUsers.ts
@@ -1,23 +1,35 @@
+import { type Static, Type } from '@sinclair/typebox';
+import { Value } from '@sinclair/typebox/value';
+
 const BASE_URL = 'https://reqres.in/';
 
-export interface User {
-	id: number;
-	email: string;
-	first_name: string;
-	last_name: string;
-	avatar: string;
-}
+const userSchema = Type.Object({
+	id: Type.Number(),
+	email: Type.String(),
+	first_name: Type.String(),
+	last_name: Type.String(),
+	avatar: Type.String(),
+});
 
-export interface UserData {
-	page: number;
-	per_page: number;
-	total: number;
-	total_pages: number;
-	data: User[];
-}
+const userDataSchema = Type.Object({
+	page: Type.Number(),
+	per_page: Type.Number(),
+	total: Type.Number(),
+	total_pages: Type.Number(),
+	data: Type.Array(userSchema),
+});
+
+export type User = Static<typeof userSchema>;
+
+export type UserData = Static<typeof userDataSchema>;
 
 export async function getUsers(page = 1): Promise<UserData> {
 	const response = await fetch(`${BASE_URL}api/users?page=${page}`);
-	const data = (await response.json()) as UserData;
+	const data: unknown = await response.json();
+
+	if (!Value.Check(userDataSchema, data)) {
+		throw new Error('Invalid user data json');
+	}
+
 	return data;
 }
